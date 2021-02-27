4b4faf58e3fac57b999516bd5228a3aa48a4501e
AlineDesmet <aline.desmet@ulb.be>
2021-02-27 20:07:36 +0100

    Problem fixed

--- src/be.ac.ulb.infof307.g06/JavaUI/sample/Controller.java
+++
@@ -1,4 +0,0 @@
-package be.ac.ulb.infof307.g06.JavaUI.sample;
-
-public class Controller {
-}
---
+++ src/be/ac/ulb/infof307/g06/Main.java
@@ -0,0 +1,24 @@
+package be.ac.ulb.infof307.g06;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        // JE NE SAIS PAS PQ MAIS CA CREE UN BUG (ALINE)!! Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("JavaUI/sample/conditions.fxml"));
+        primaryStage.setTitle("Terms & conditions");
+        primaryStage.setScene(new Scene(root,600, 500));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
