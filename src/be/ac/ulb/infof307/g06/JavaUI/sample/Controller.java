22c146658de2fd124b490efe081b97b0a44ac696
dibenite <33431271+dibenite@users.noreply.github.com>
2021-02-28 14:18:41 +0100

    Update Controller.java

--- src/be/ac/ulb/infof307/g06/JavaUI/sample/Controller.java
+++ src/be/ac/ulb/infof307/g06/JavaUI/sample/Controller.java
@@ -1,4 +1,26 @@
 package be.ac.ulb.infof307.g06.JavaUI.sample;
 
+import com.gluonhq.charm.glisten.control.TextField;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.Ellipse;
+
 public class Controller {
+
+    @FXML
+    private AnchorPane root;
+
+    @FXML
+    private Button ConnectionButton;
+
+    @FXML
+    private TextField Password;
+
+    @FXML
+    private TextField Username;
+
+    @FXML
+    private Ellipse Ellipse;
+
 }
