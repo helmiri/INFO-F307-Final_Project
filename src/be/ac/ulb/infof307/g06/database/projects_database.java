c7c1908348485995f4a78dc699b6b1d1bc54f7ed
Luca <desantos_luca@yahoo.com>
2021-03-01 19:21:36 +0100

    Added subproject and tasks database

--- src/be/ac/ulb/infof307/g06/database/projects_database.java
+++ src/be/ac/ulb/infof307/g06/database/projects_database.java
@@ -1,26 +1,31 @@
 package be.ac.ulb.infof307.g06.database;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 class Project{
     int id;
     String title;
     String description;
     String tags;
     Date date;
-    int[] subProjects;
-    int[] tasks;
+    int parent_id;
 
-    public Project(int id, String title, String description, String tags, Date date){
+    public Project(int id, String title, String description, String tags, Date date, int parent_id){
         this.title = title;
         this.description = description;
         this.tags = tags;
         this.date = date;
+        this.parent_id = parent_id;
     }
+
     public int getId(){return id;}
     public String getTitle(){return title;}
     public String getDescription(){return description;}
     public String getTags(){return tags;}
     public Date getDate(){return date;}
+    public int getParent_id(){return parent_id;}
 }
 
 public class projects_database {
@@ -28,55 +33,49 @@ public class projects_database {
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         init("projects.db");
-        createTable();
-        createProject("test1", "decr1", "tags1");
-        System.out.println(getProject(0).getTitle());
-        System.out.println(getProjectID("test1"));
+        Date today = new Date(System.currentTimeMillis());
+        //createTask("test1", 1);
+        //createTask("test2", 1);
+        //createTask("test3", 1);
+        //createTask("test4", 2);
+
+        System.out.println(getTasks(1).toString());
     }
 
-    private static void init(String dbName) throws SQLException, ClassNotFoundException {
+    public static void init(String dbName) throws SQLException, ClassNotFoundException {
         Class.forName("org.sqlite.JDBC");
         database = DriverManager.getConnection("jdbc:sqlite:" + dbName);
+        createTable();
     }
 
     private static void createTable() throws SQLException {
         Statement state = database.createStatement();
-        //TODO: Check if table already exists
 
-        ResultSet res1 = state.executeQuery("SELECT name FROM sqlite_master WHERE type='table' AND name='Project'");
-        ResultSet res2 = state.executeQuery("SELECT name FROM sqlite_master WHERE type='table' AND name='Collaborator'");
-        ResultSet res3 = state.executeQuery("SELECT name FROM sqlite_master WHERE type='table' AND name='Task'");
-        ResultSet res4 = state.executeQuery("SELECT name FROM sqlite_master WHERE type='table' AND name='Subproject'");
-        System.out.println(res1.next());
-        if (res1.next()) {System.out.println("exists");} else {System.out.println("doesnt exists");}
-        
-        if (res2==null) {
-            state.execute("CREATE TABLE Collaborator(id Integer, project_id Integer, user_id Integer);");
-        }
-        if (res3==null) {
-            state.execute("CREATE TABLE Task(id Integer, description varchar(20), project_id Integer);");
-        }
-        if (res4==null) {
-            state.execute("CREATE TABLE SubProject(id Integer, project_id Integer);");
-        }
+        state.execute("CREATE TABLE IF NOT EXISTS Project(id Integer, title varchar(20), description varchar(20), tags varchar(20), date Long, parent_id Integer);");
+        state.execute("CREATE TABLE IF NOT EXISTS Collaborator(id Integer, project_id Integer, user_id Integer);");
+        state.execute("CREATE TABLE IF NOT EXISTS Task(id Integer, description varchar(20), project_id Integer);");
+
     }
 
-    private static int createProject(String title, String description, String tags) throws SQLException {
+    public static int createProject(String title, String description, String tags, Date date, int parent_id) throws SQLException {
         Statement state = database.createStatement();
         int id;
         try {
-            ResultSet rs = state.executeQuery("SELECT MAX(id) FROM Project;");
+            ResultSet rs = state.executeQuery("SELECT id, MAX(id) FROM Project;");
             id = rs.getInt("id");
             id++;
         } catch (Exception e) {
-            id = 0;
+            System.out.println(e);
+            id = 1;
         }
-        state.execute("INSERT INTO Project (id, title, description, tags) VALUES('" + id + "','" + title + "','" + description + "','" + tags + "');");
+
+        state.execute("INSERT INTO Project (id, title, description, tags, date, parent_id) VALUES('" +
+                id + "','" + title + "','" + description + "','" + tags + "','" + date.getTime() + "','" + parent_id + "');");
 
         return id;
     }
 
-    private static int getProjectID(String title)throws  SQLException{
+    public static int getProjectID(String title) throws SQLException{
         Statement state = database.createStatement();
         ResultSet rs = state.executeQuery("SELECT id FROM Project WHERE title='" + title + "';");
 
@@ -85,15 +84,45 @@ public class projects_database {
         return id;
     }
 
-    private static Project getProject(int id) throws SQLException {
+    public static Project getProject(int id) throws SQLException {
         Statement state = database.createStatement();
-        ResultSet rs = state.executeQuery("SELECT title,description,tags, date FROM Project WHERE id='" + id + "';");
+        ResultSet rs = state.executeQuery("SELECT title,description,tags,date,parent_id FROM Project WHERE id='" + id + "';");
 
         String title = rs.getString("title");
         String description = rs.getString("description");
         String tags = rs.getString("tags");
-        Date date =  rs.getDate("date");
-        Project res = new Project(id,title,description,tags,date);
+        Date date =  new Date(rs.getLong("date"));
+        int parent_id = rs.getInt("parent_id");
+        Project res = new Project(id,title,description,tags,date,parent_id);
+
         return res;
     }
+
+    public static int createTask(String description, int project_id) throws SQLException{
+        Statement state = database.createStatement();
+        int id;
+        try {
+            ResultSet rs = state.executeQuery("SELECT id, MAX(id) FROM Task;");
+            id = rs.getInt("id");
+            id++;
+        } catch (Exception e) {
+            System.out.println(e);
+            id = 1;
+        }
+        state.execute("INSERT INTO Task (id, description, project_id) VALUES('" +
+                id + "','" + description + "','" + project_id+ "');");
+        return id;
+    }
+
+    public static List<String> getTasks(int project_id) throws SQLException{
+        Statement state = database.createStatement();
+        ResultSet rs = state.executeQuery("SELECT description FROM Task WHERE project_id='" + project_id + "';");
+        List<String> res = new ArrayList<String>();
+
+        while (rs.next()){
+            res.add(rs.getString("description"));
+        }
+        return res;
+    }
+
 }
