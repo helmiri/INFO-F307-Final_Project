d3b1c5f017f3f04dc6e6a606c04219cb5075ded6
dibenite <facedechips98@live.fr>
2021-03-03 15:38:05 +0100

    Fix des fenêtres et liaison entre elles, conditions utilisateurs pas encore ouf

--- src/be/ac/ulb/infof307/g06/JavaUI/sample/Controller.java
+++ src/be/ac/ulb/infof307/g06/JavaUI/sample/Controller.java
@@ -1,38 +1,104 @@
 package be.ac.ulb.infof307.g06.JavaUI.sample;
 
-import javafx.application.Application;
+
+import be.ac.ulb.infof307.g06.Main;
 import com.gluonhq.charm.glisten.control.TextField;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.control.Button;
-import javafx.scene.layout.AnchorPane;
-import org.controlsfx.control.ToggleSwitch;
+import javafx.animation.PauseTransition;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
+import javafx.util.Duration;
+import org.controlsfx.control.ToggleSwitch;
 
 public class Controller {
+    private Main main;
 
     @FXML
-    private AnchorPane root;
+    private Text helloUser;
 
     @FXML
-    private TextField Username;
+    private TextField username;
 
     @FXML
-    private TextField Password;
+    private PasswordField password;
 
     @FXML
-    private Button ConnectionButton;
+    private Button connectionBtn;
 
     @FXML
-    private Button register_btn;
+    private Button registerBtn;
 
     @FXML
     private ToggleSwitch toggle_switch;
 
     @FXML
-    private void buttonEvents(ActionEvent event) {
+    private TextField firstName;
+
+    @FXML
+    private TextField lastName;
+
+    @FXML
+    private TextField emailAddress;
+
+    @FXML
+    private TextField usernameRegister;
+
+    @FXML
+    private TextField registerPassword;
+
+    @FXML
+    private TextField passwordConfirmation;
+
+    @FXML
+    private Button SignUpBtn;
+
+    @FXML
+    private Button goToConnection;
+
+    @FXML
+    private Button acceptConditionsBtn;
+
+    @FXML
+    private CheckBox acceptConditionsBox;
+
+
+    @FXML
+    private void buttonEvents(ActionEvent event) throws Exception {
+        if( event.getSource()== connectionBtn){
+            String passwd;
+            String user = username.getText();
+            passwd = password.getText();
+            mainMenu(event, passwd, user);
+        }
+
+        else if( event.getSource()== SignUpBtn     )  {
+            main.showConditionsScene();
+        }
+
+        else if( event.getSource()== registerBtn   )  {main.showRegisterScene()  ;}
+        else if( event.getSource()== goToConnection)  {main.showConnectionScene();}
+        else if( event.getSource()== acceptConditionsBtn){
+            if(acceptConditionsBox.isSelected()) {
+                main.showStatisticsScene();
+
+            }
+        }
 
     }
 
+    @FXML
+    private void mainMenu(ActionEvent event, String passwd, String user) throws Exception {
+        /*
+            Creates a delay to say hello to the user before going to the main menu.
+         */
+        PauseTransition delay = new PauseTransition(Duration.seconds(0.3));
+        delay.setOnFinished(actionEvent -> helloUser.setText("Bienvenue " + user + "!"));
+        delay.play();
+        main.showStatisticsScene();
+
+    }
 }
--- src/be/ac/ulb/infof307/g06/Main.java
+++ src/be/ac/ulb/infof307/g06/Main.java
@@ -1,18 +1,77 @@
 package be.ac.ulb.infof307.g06;
 
+
+import javafx.animation.PauseTransition;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 public class Main extends Application {
+    private static Stage primaryStage;
+    private static AnchorPane mainLayout;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("JavaUI/sample/conditions.fxml")); // Test
-        primaryStage.setScene(new Scene(root,1500, 900));
+        Main.primaryStage = primaryStage;
+        Main.primaryStage.setTitle("Projet génie logiciel");
+        // SetUp the main page. Should try to change that.
+        FXMLLoader loader =  new FXMLLoader();
+        loader.setLocation(Main.class.getResource("JavaUI/sample/Connection.fxml"));
+        mainLayout = loader.load();
+        Scene scene = new Scene(mainLayout);
+        primaryStage.setScene(scene);
         primaryStage.show();
+        /*
+            If u want to show another window as the first one u need to get rif of
+            the "setup" part and the "delay" part and replace them by:
+            mainLayout = loader.load();
+            Scene scene = new Scene(mainLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+         */
+    }
+
+    public static void showConnectionScene() throws Exception{
+        FXMLLoader loader =  new FXMLLoader();
+        loader.setLocation(Main.class.getResource("JavaUI/sample/Connection.fxml"));
+        // Setup the new page.
+        AnchorPane connection = loader.load();
+        mainLayout.getChildren().setAll(connection);
+    }
+
+    public static void showRegisterScene() throws Exception{
+        FXMLLoader loader =  new FXMLLoader();
+        loader.setLocation(Main.class.getResource("JavaUI/sample/Register.fxml"));
+        // Setup the new page.
+        AnchorPane register = loader.load();
+        mainLayout.getChildren().setAll(register);
+    }
+
+    public static void showStatisticsScene() throws Exception{
+        FXMLLoader loader =  new FXMLLoader();
+        loader.setLocation(Main.class.getResource("JavaUI/sample/statistics1.fxml"));
+        // Setup the new page.
+        AnchorPane register = loader.load();
+        // Delay part.
+        PauseTransition delay = new PauseTransition(Duration.seconds(1.5));
+        delay.setOnFinished( actionEvent ->mainLayout.getChildren().setAll(register));
+        delay.play();
+    }
+
+    public static void showConditionsScene() throws Exception{
+        FXMLLoader loader =  new FXMLLoader();
+        loader.setLocation(Main.class.getResource("JavaUI/sample/Conditions.fxml"));
+        AnchorPane conditions = loader.load();
+        Stage stage = new Stage();
+        stage.setTitle("User terms and conditions");
+        stage.setScene(new Scene(conditions, 750, 900));
+        stage.show();
     }
 
     public static void main(String[] args) {
