a91fbb7110ce4605a703efa14e597fa791d32ac2
helmiri <Hamza.El.Miri@ulb.be>
2021-03-08 13:14:22 +0100

    Added javadoc for Database and UserDB

---
+++ docs/package-search-index.js
@@ -0,0 +1,5 @@
+packageSearchIndex = [{
+    "l": "All Packages",
+    "u": "allpackages-index.html"
+}, {"l": "be.ac.ulb.infof307.g06"}, {"l": "be.ac.ulb.infof307.g06.database"}, {"l": "be.ac.ulb.infof307.g06.JavaUI.sample"}];
+updateSearchResults();
