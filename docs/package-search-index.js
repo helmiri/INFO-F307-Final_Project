fc22428d30acd6f310fdb35216130e5cacb45cae
AlineDesmet <aline.desmet@ulb.be>
2021-03-08 23:11:05 +0100

    JavaDOC

--- docs/package-search-index.js
+++ docs/package-search-index.js
@@ -1,5 +1,1 @@
-packageSearchIndex = [{
-    "l": "All Packages",
-    "u": "allpackages-index.html"
-}, {"l": "be.ac.ulb.infof307.g06"}, {"l": "be.ac.ulb.infof307.g06.database"}, {"l": "be.ac.ulb.infof307.g06.JavaUI.sample"}];
-updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"be.ac.ulb.infof307.g06"},{"l":"be.ac.ulb.infof307.g06.database"},{"l":"be.ac.ulb.infof307.g06.JavaUI.sample"}];updateSearchResults();
