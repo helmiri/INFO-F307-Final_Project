cf5c22d6589440bb35a95f17ec259dc462f9010c
helmiri <Hamza.El.Miri@ulb.be>
2021-03-08 23:28:45 +0100

    removed

--- compile_exec.bat
+++
@@ -1,4 +0,0 @@
-
-C:\Users\chsen\.jdks\openjdk-15.0.2\bin\javac.exe --module-path lib/javafx --add-modules javafx.controls,javafx.fxml .\src\be\ac\ulb\infof307\g06\Main.java .\src\be\ac\ulb\infof307\g06\database\*.java .\src\be\ac\ulb\infof307\g06\JavaUI\sample\*.java -d .\out\production\2021-groupe-6\ && copy .\src\be\ac\ulb\infof307\g06\JavaUI\sample\*.fxml .\out\production\2021-groupe-6\be\ac\ulb\infof307\g06\JavaUI\sample\
-C:\Users\chsen\.jdks\openjdk-15.0.2\bin\java.exe --module-path lib/javafx --add-modules javafx.controls,javafx.fxml -classpath ".\out\production\2021-groupe-6;.\lib\junit\*.jar;.\lib\controlsfx\*.jar;.\lib\javafx\*.jar;.\lib\sqlite\sqlite-jdbc-3.34.0.jar" be.ac.ulb.infof307.g06.Main
-
