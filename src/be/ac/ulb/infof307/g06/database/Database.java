c197a6448f0b6d5a5eb2a15b22359b723d194648
dibenite <facedechips98@live.fr>
2021-03-20 19:10:42 +0100

    fix database

--- src/be/ac/ulb/infof307/g06/database/Database.java
+++ src/be/ac/ulb/infof307/g06/database/Database.java
@@ -62,7 +62,7 @@ public abstract class Database {
         state.execute("CREATE TABLE IF NOT EXISTS Task(id Integer, description varchar(20), project_id Integer);");
         state.execute("CREATE TABLE IF NOT EXISTS Tag(id Integer, description varchar(20), color Integer);");
         state.execute("CREATE TABLE IF NOT EXISTS Tag_projects(tag_id Integer, project_id Integer);");
-        state.execute("CREATE TABLE IF NOT EXISTS users(id Integer, fName varchar(20), lName varchar(20), userName varchar(20),email varchar(40),password varchar(20), primary key (id));");
+        state.execute("CREATE TABLE IF NOT EXISTS users(id Integer, fName varchar(20), lName varchar(20), userName varchar(20),email varchar(40),password varchar(20), status boolean, primary key (id));");
         close(state);
     }
 }
