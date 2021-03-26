7d7371a43cb01a6d9e88f011e0c018505d1f53e1
helmiri <Hamza.El.Miri@ulb.be>
2021-03-26 22:15:25 +0100

    Adapt test
    Signed-off-by: helmiri <Hamza.El.Miri@ulb.be>

--- test/be/ac/ulb/infof307/g06/TestUserDB.java
+++ test/be/ac/ulb/infof307/g06/TestUserDB.java
@@ -110,7 +110,7 @@ public class TestUserDB extends TestDatabase {
     @Test
     @DisplayName("Add access token")
     public void testAddAccToken() throws SQLException {
-        UserDB.addAccToken("Random_Token_String", "CLIENTID", "User_1_userName");
+        UserDB.addAccToken("Random_Token_String", "CLIENTID");
         Statement state = db.createStatement();
         ResultSet res = state.executeQuery("SELECT accToken from users where userName='User_1_userName'");
         String token = res.getString("accToken");
