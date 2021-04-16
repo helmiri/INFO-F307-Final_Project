1d169ee4f58fea83af23e90c48b371f2676dba30
Luca <desantos_luca@yahoo.com>
2021-04-16 14:53:57 +0200

    calendarController progress

--- src/be/ac/ulb/infof307/g06/controllers/CalendarController.java
+++ src/be/ac/ulb/infof307/g06/controllers/CalendarController.java
@@ -2,25 +2,25 @@ package be.ac.ulb.infof307.g06.controllers;
 
 import be.ac.ulb.infof307.g06.models.database.ProjectDB;
 import be.ac.ulb.infof307.g06.models.database.UserDB;
+
 import be.ac.ulb.infof307.g06.views.CalendarViewController;
 import com.calendarfx.model.Calendar;
 import com.calendarfx.model.CalendarSource;
 import com.calendarfx.model.Entry;
 import com.calendarfx.view.AllDayView;
 import com.calendarfx.view.WeekDayHeaderView;
-import com.calendarfx.view.page.WeekPage;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.time.LocalDate;
-import java.time.LocalTime;
 
 public class CalendarController extends Controller implements CalendarViewController.ViewListener {
     private CalendarViewController viewController;
-    private AllDayView view;
+    private AllDayView allDay;
     private CalendarSource source;
-    private WeekDayHeaderView week;
+    private WeekDayHeaderView header;
     private LocalDate currentDate;
 
     public CalendarController(UserDB user_db, ProjectDB project_db, Stage stage, Scene scene) {
@@ -44,36 +44,42 @@ public class CalendarController extends Controller implements CalendarViewContro
 
     @Override
     public void show() throws IOException, SQLException {
-        view = new AllDayView();
         source = new CalendarSource("calendars");
-        view.getCalendarSources().setAll(source);
-        view.setAdjustToFirstDayOfWeek(false);
-        view.setRowHeight(50);
-        viewController = new CalendarViewController();
+
+
+        FXMLLoader loader = new FXMLLoader(CalendarViewController.class.getResource("CalendarView.fxml"));
+        try {
+            scene = new Scene(loader.load());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        viewController = loader.getController();
         viewController.setListener(this);
-        scene = new Scene(view, 1515, 940);
         stage.setScene(scene);
         stage.sizeToScene();
-        stage.show();
-        createEntry("test1", LocalDate.now(), LocalDate.now().plusDays(10), "blue");
-        createEntry("test2", LocalDate.now().plusDays(2), LocalDate.now().plusDays(12), "red");
+        allDay = (AllDayView) scene.lookup("allDayViewID");
+        allDay.getCalendarSources().setAll(source);
+        allDay.setAdjustToFirstDayOfWeek(false);
+        allDay.setRowHeight(50);
+        header = (WeekDayHeaderView) scene.lookup("weekDays");
+        createEntry("test", LocalDate.now(), LocalDate.now(), "blue");
     }
 
     @Override
     public void nextWeek() {
-        week.setDate(currentDate.plusDays(7));
-        view.setDate(currentDate.plusDays(7));
+        header.setDate(currentDate.plusDays(7));
+        allDay.setDate(currentDate.plusDays(7));
     }
 
     @Override
     public void prevWeek() {
-        week.setDate(currentDate.minusDays(7));
-        view.setDate(currentDate.minusDays(7));
+        header.setDate(currentDate.minusDays(7));
+        allDay.setDate(currentDate.minusDays(7));
     }
 
     @Override
     public void goToday() {
-        week.setDate(LocalDate.now());
-        view.setDate(LocalDate.now());
+        header.setDate(LocalDate.now());
+        allDay.setDate(LocalDate.now());
     }
 }
