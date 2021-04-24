ce1f442feed475a18774e1febbe012a9ef456110
jdebilde <jean-jacques.debilde@ulb.be>
2021-04-24 21:00:41 +0200

    add java doc in IOController

--- src/be/ac/ulb/infof307/g06/controllers/projectControllers/IOController.java
+++ src/be/ac/ulb/infof307/g06/controllers/projectControllers/IOController.java
@@ -69,7 +69,7 @@ public class IOController extends Controller {
      */
     private void saveProjectAndChildsJson(Project project, FileWriter fw) throws IOException {
         try {
-            final int ID = project.getId();
+            int ID = project.getId();
             saveProjectJson(project, project_db.getTasks(ID), project_db.getTags(ID), fw);
             for (Integer subProject : project_db.getSubProjects(ID)) {
                 fw.write(",\n");
@@ -89,7 +89,6 @@ public class IOController extends Controller {
     public void onExportProject(Project project, String archivePath) {
         try {
             String jsonFile = archivePath + "/file.json";
-            final int ID = project.getId();
             deleteFile(jsonFile);
             FileWriter fw = new FileWriter(jsonFile, true);
             fw.write("[\n");
@@ -101,7 +100,6 @@ public class IOController extends Controller {
             new AlertWindow("Success", "Success in project export." ).informationWindow();
         } catch (Exception e) {
             new AlertWindow("Failure", "Failure to export project : " + e).errorWindow();
-            // TODO Exception
         }
     }
 
@@ -140,17 +138,17 @@ public class IOController extends Controller {
             unzip(archivePath, directory);
             if (isProjectInDb(jsonFile)) return;
             BufferedReader reader = new BufferedReader(new FileReader(jsonFile));
-            int count = 0, idParent = 0 ,id = 0;
-            String line=reader.readLine();
+            int count = 0, parentID = 0 ,id = 0;
+            String line = reader.readLine();
             Map<Integer, Integer> hm = new HashMap();
             while ((line = reader.readLine()) != null) {
                 ++count;
                 count %= 6;
                 switch (count) {
                     case 2:
-                        int []listIdIdParent=  parseProject(id , idParent , hm,line);
-                        id = listIdIdParent[0];
-                        idParent = listIdIdParent[1];
+                        int[] listIdparentID=  parseProject(parentID, hm, line);
+                        id = listIdparentID[0];
+                        parentID = listIdparentID[1];
                         break;
                     case 3:
                         parseTasks(id, line);
@@ -160,13 +158,10 @@ public class IOController extends Controller {
                         // update la view
                         TreeItem<Project> child = new TreeItem<>(project_db.getProject(id));
                         setViewController(viewController);
-                        viewController.insertProject(id, child, idParent);
-                        //ioController = new IOController(user_db, project_db, stage, scene);
-                        //ioController.setViewController(viewController);
+                        viewController.insertProject(id, child, parentID);
                         break;
                     default:
                         if (line.equals("[")) {count = 1;}
-
                 }
             }
             user_db.updateDiskUsage(project_db.getSizeOnDisk());
@@ -179,42 +174,61 @@ public class IOController extends Controller {
         }
     }
 
+    /**
+     *  Parse a line of json file containing a list of tags.
+     *  Create the tags and add them to the project.
+     * @param id int
+     * @param line String
+     * @throws SQLException
+     */
     private void parseTags(int id, String line) throws SQLException {
-        int idTag;
-        Type listkind = new TypeToken<ArrayList<Tag>>() {
-        }.getType();
-        List<Tag> tag = new Gson().fromJson(line, listkind);
+        int tagID;
+        Type listType = new TypeToken<ArrayList<Tag>>() {}.getType();
+        List<Tag> tag = new Gson().fromJson(line, listType);
         for (Tag t : tag) {
-            //verifier dans la bdd
-            idTag = project_db.getTagID(t.getDescription());
-            if (idTag == 0) {
-                idTag = project_db.createTag(t.getDescription(), t.getColor());
-            }
-            project_db.addTag(idTag, id);
+            tagID = project_db.createTag(t.getDescription(), t.getColor());
+            project_db.addTag(tagID, id);
         }
     }
 
+    /**
+     * Parse a line of json file containing a list of tasks.
+     * Create the tasks and add them to the project.
+     * @param id int
+     * @param line String
+     * @throws SQLException
+     */
     private void parseTasks(int id, String line) throws SQLException {
-        Type listType = new TypeToken<ArrayList<Task>>() {
-        }.getType();
+        Type listType = new TypeToken<ArrayList<Task>>() {}.getType();
         List<Task> tasks = new Gson().fromJson(line.substring(0, line.length() - 1), listType);
         for (Task t : tasks) {
             project_db.createTask(t.getDescription(), id, t.getStartDate(), t.getEndDate());
         }
     }
-    private int[] parseProject(int id , int idParent , Map<Integer,Integer> hm,String line) throws SQLException {
+
+    /**
+     * Parse a line of json file containing a project (Object of type Project).
+     * Create the project and add it to the database.
+     * @param parentID int
+     * @param hm Map<Integer,Integer>
+     * @param line String
+     * @return A list with of 2 integers, first one is the project id and
+     *         second one is his parent's project id
+     * @throws SQLException
+     */
+    private int[] parseProject(int parentID , Map<Integer,Integer> hm,String line) throws SQLException {
         Project project = new Gson().fromJson(line.substring(0, line.length() - 1), Project.class);
+        int id;
         if (hm.size() == 0) {
             id = project_db.createProject(project.getTitle(), project.getDescription(), project.getStartDate(), project.getEndDate(), 0);
             hm.put(project.getParent_id(), 0);
         } else {
             id = project_db.createProject(project.getTitle(), project.getDescription(), project.getStartDate(), project.getEndDate(), hm.get(project.getParent_id()));
-            idParent = hm.get(project.getParent_id());
+            parentID = hm.get(project.getParent_id());
         }
         hm.put(project.getId(), id);
         project_db.addCollaborator(id, user_db.getCurrentUser().getId());
-        return new int []{ id, idParent};
-
+        return new int []{id, parentID};
     }
 
     /**
@@ -242,7 +256,7 @@ public class IOController extends Controller {
      * @param source      String
      * @param destination String
      */
-    public void unzip(final String source, final String destination) {
+    public void unzip(String source, String destination) {
         try {
             Archiver archiver =
                     ArchiverFactory.createArchiver(ArchiveFormat.TAR, CompressionType.GZIP);
@@ -270,17 +284,13 @@ public class IOController extends Controller {
             while ((line = reader.readLine()) != null) {
                 ++count;
                 count %= 6;
-                switch (count) {
-                    case 2:
-                        System.out.println("projet " + line);
-                        Project project = new Gson().fromJson(line.substring(0, line.length() - 1), Project.class);
-                        int id = project_db.getProjectID(project.getTitle());
-                        if (id != 0) return true;
-                        break;
-
-                    default:
-                        if (line.equals("[")) { count = 1; }
+                if (count == 2) {
+                    System.out.println("projet " + line);
+                    Project project = new Gson().fromJson(line.substring(0, line.length() - 1), Project.class);
+                    int id = project_db.getProjectID(project.getTitle());
+                    if (id != 0) return true;
                 }
+                else { if (line.equals("[")) count = 1; }
             }
             reader.close();
             return false;
@@ -288,12 +298,13 @@ public class IOController extends Controller {
             return false;
         }
     }
+    
     /**
      * Delete a file.
      *
      * @param fileName String
      */
-    public void deleteFile(final String fileName) {
+    public void deleteFile(String fileName) {
         try {
             File myObj = new File(fileName);
             myObj.delete();
