eb2ac6be00fa0b60de26e9d6508f4f005b8ce46a
helmiri <Hamza.El.Miri@ulb.be>
2021-04-26 21:02:17 +0200

    Updated compilation/execution scripts
    Signed-off-by: helmiri <Hamza.El.Miri@ulb.be>

--- compile.bat
+++ compile.bat
@@ -10,13 +10,19 @@ dir /s/b *.java > ..\compile\build\sources.txt
 cd ..
 xcopy .\src .\compile\build /t/e
 
-%1 -cp .\lib\dropbox\dropbox-core-sdk-3.1.5.jar;.\lib\dropbox\jackson-core-2.7.4.jar;.\lib\gson\gson-2.8.6.jar;.\lib\jarchivelib\jarchivelib-0.7.1-jar-with-dependencies.jar;.\lib\controlsfx\controlsfx-11.1.0.jar --module-path lib\javafx\windows\lib --add-modules javafx.controls,javafx.fxml @.\compile\build\sources.txt .\src\be\ac\ulb\infof307\g06\Controllers\MainController.java -d .\compile\build
+%1 -cp ".\lib\calendarfx\view-11.8.3.jar;.\lib\googledrive\*;.\lib\dropbox\dropbox-core-sdk-3.1.5.jar;.\lib\dropbox\jackson-core-2.7.4.jar;.\lib\gson\gson-2.8.6.jar;.\lib\jarchivelib\jarchivelib-0.7.1-jar-with-dependencies.jar;.\lib\controlsfx\controlsfx-11.1.0.jar" --module-path lib\javafx\windows\lib --add-modules javafx.controls,javafx.fxml @.\compile\build\sources.txt .\src\be\ac\ulb\infof307\g06\controllers\connectionControllers\ConnectionEngine.java -d .\compile\build
 
 copy .\src\be\ac\ulb\infof307\g06\views\*.fxml .\compile\build\be\ac\ulb\infof307\g06\views\
-copy .\src\be\ac\ulb\infof307\g06\views\ConnectionsViews\*.fxml .\compile\build\be\ac\ulb\infof307\g06\views\ConnectionsViews\
-copy .\src\be\ac\ulb\infof307\g06\views\ProjectViews\*.fxml .\compile\build\be\ac\ulb\infof307\g06\views\ProjectViews\
-copy .\src\be\ac\ulb\infof307\g06\views\StatisticsViews\*.fxml .\compile\build\be\ac\ulb\infof307\g06\views\StatisticsViews\
+copy .\src\be\ac\ulb\infof307\g06\views\connectionViews\*.fxml .\compile\build\be\ac\ulb\infof307\g06\views\connectionViews\
+copy .\src\be\ac\ulb\infof307\g06\views\projectViews\*.fxml .\compile\build\be\ac\ulb\infof307\g06\views\projectViews\
+copy .\src\be\ac\ulb\infof307\g06\views\projectViews\*.css .\compile\build\be\ac\ulb\infof307\g06\views\projectViews\
+copy .\src\be\ac\ulb\infof307\g06\views\projectViews\popups\*.fxml .\compile\build\be\ac\ulb\infof307\g06\views\projectViews\popups\
+copy .\src\be\ac\ulb\infof307\g06\views\statisticsViews\*.fxml .\compile\build\be\ac\ulb\infof307\g06\views\statisticsViews\
 copy .\src\be\ac\ulb\infof307\g06\icons\* .\compile\build\be\ac\ulb\infof307\g06\icons\
+copy .\src\be\ac\ulb\infof307\g06\views\calendarViews\*.fxml .\compile\build\be\ac\ulb\infof307\g06\views\calendarViews\
+copy .\src\be\ac\ulb\infof307\g06\views\mainMenuViews\*.fxml .\compile\build\be\ac\ulb\infof307\g06\views\mainMenuViews\
+copy .\src\be\ac\ulb\infof307\g06\views\settingsViews\*.fxml .\compile\build\be\ac\ulb\infof307\g06\views\settingsViews\
+
 
 
 
