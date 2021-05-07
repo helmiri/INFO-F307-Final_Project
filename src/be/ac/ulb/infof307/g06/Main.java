dc6de31196dd27dafff1f4805dc625ab3b7a816c
helmiri <Hamza.El.Miri@ulb.be>
2021-05-07 21:14:17 +0200

    Handle database exception
    Signed-off-by: helmiri <Hamza.El.Miri@ulb.be>

--- src/be/ac/ulb/infof307/g06/Main.java
+++ src/be/ac/ulb/infof307/g06/Main.java
@@ -1,7 +1,7 @@
 package be.ac.ulb.infof307.g06;
 
 import be.ac.ulb.infof307.g06.controllers.connectionControllers.ConnectionHandler;
-import be.ac.ulb.infof307.g06.models.AlertWindow;
+import be.ac.ulb.infof307.g06.exceptions.DatabaseException;
 import be.ac.ulb.infof307.g06.models.database.ProjectDB;
 import be.ac.ulb.infof307.g06.models.database.UserDB;
 import javafx.application.Application;
@@ -32,11 +32,10 @@ public class Main extends Application {
             UserDB userDB = new UserDB(DB_PATH);
             ProjectDB projectDB = new ProjectDB(DB_PATH);
             boolean isFirstBoot = userDB.isFirstBoot();
-            ConnectionHandler engine = new ConnectionHandler(userDB, projectDB, stage, isFirstBoot);
-            engine.showLogin();
-        } catch (SQLException | ClassNotFoundException throwables) {
-            new AlertWindow("Database error", "Could not access the database").errorWindow();
+            ConnectionHandler handler = new ConnectionHandler(userDB, projectDB, stage, isFirstBoot);
+            handler.showLogin();
+        } catch (SQLException | ClassNotFoundException e) {
+            new DatabaseException(e).show();
         }
-
     }
 }
