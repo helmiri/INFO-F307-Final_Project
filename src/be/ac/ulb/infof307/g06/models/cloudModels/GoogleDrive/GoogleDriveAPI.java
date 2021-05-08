72dd6c9071ca939f154559c1636ad79443534a1a
helmiri <Hamza.El.Miri@ulb.be>
2021-05-08 23:43:02 +0200

    Added update method
    Signed-off-by: helmiri <Hamza.El.Miri@ulb.be>

--- src/be/ac/ulb/infof307/g06/models/cloudModels/GoogleDrive/GoogleDriveAPI.java
+++ src/be/ac/ulb/infof307/g06/models/cloudModels/GoogleDrive/GoogleDriveAPI.java
@@ -39,8 +39,8 @@ public class GoogleDriveAPI {
     /**
      * Returns the files contained in the GCloud account.
      *
-     * @return
-     * @throws IOException
+     * @return A list of the files MetaDatas
+     * @throws IOException on error accessing the files
      */
     public List<File> getFiles() throws IOException {
         FileList result = service.files().list().setFields("nextPageToken, files(id, name, appProperties)").execute();
@@ -52,14 +52,13 @@ public class GoogleDriveAPI {
      *
      * @param localFilePath Path of the folder we want to download our file in.
      * @param fileID        Id of the file in the Gcloud account we want to download.
-     * @throws IOException
+     * @throws IOException On error saving the file
      */
     public void downloadFile(String localFilePath, String fileID) throws IOException {
         OutputStream outputStream = new FileOutputStream(localFilePath);
         service.files().get(fileID).executeMediaAndDownloadTo(outputStream);
         outputStream.close();
     }
-
 
     /**
      * Uploading a file to the cloud services.
@@ -77,12 +76,26 @@ public class GoogleDriveAPI {
     }
 
     /**
-     * Adds the hash code to the metadatafile aan "attribe" so that we can retrieve it later.
+     * Deletes an existing file and replaces it with a new one
+     *
+     * @param localFilePath Path to the file to be uploaded
+     * @param cloudFilePath Path to the file to be updated
+     * @param file          The cloud file's metadata
+     * @throws IOException On error updating the file
+     */
+    public void updateFile(String localFilePath, String cloudFilePath, File file) throws IOException {
+        uploadFile(localFilePath, cloudFilePath);
+        service.files().delete(file.getId()).execute();
+    }
+
+    /**
+     * Adds the hash code to the metadata of the file so it can be retrieved on download because the Google drive
+     * API doesn't offer a way to easily retrieve it
      *
      * @param cloudFilePath Path to the file contained in the cloud
      * @param fileMetadata  The metadata file
-     * @param localFile     The localfile
-     * @throws IOException
+     * @param localFile     The local file
+     * @throws IOException On error reading the file
      */
     private void setHashCode(String cloudFilePath, File fileMetadata, java.io.File localFile) throws IOException {
         Map<String, String> properties = new HashMap<>();
@@ -95,8 +108,8 @@ public class GoogleDriveAPI {
      * Returns the hash code of a file.
      *
      * @param file The file that we want to get the hash content from.
-     * @return
-     * @throws IOException
+     * @return The hash code
+     * @throws IOException On error reading the file
      */
     @SuppressWarnings("UnstableApiUsage")
     public String getHash(java.io.File file) throws IOException {
