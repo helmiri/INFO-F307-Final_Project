cf1b6101d587c8f3fe2acec55183e814fd7371de
helmiri <Hamza.El.Miri@ulb.be>
2021-05-08 23:40:52 +0200

    Removed deprecated api usage
    Signed-off-by: helmiri <Hamza.El.Miri@ulb.be>

--- src/be/ac/ulb/infof307/g06/models/cloudModels/DropBox/DropBoxAPI.java
+++ src/be/ac/ulb/infof307/g06/models/cloudModels/DropBox/DropBoxAPI.java
@@ -2,18 +2,19 @@ package be.ac.ulb.infof307.g06.models.cloudModels.DropBox;
 
 import com.dropbox.core.DbxException;
 import com.dropbox.core.DbxRequestConfig;
+import com.dropbox.core.http.StandardHttpRequestor;
 import com.dropbox.core.v2.DbxClientV2;
 import com.dropbox.core.v2.files.*;
 
 import java.io.*;
 import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 
 public class DropBoxAPI {
-    private final DbxClientV2 dboxClient;
+    private final DbxClientV2 dropboxClient;
 
     /**
      * Initializes the connection with the dropbox account.
@@ -23,18 +24,23 @@ public class DropBoxAPI {
      */
     public DropBoxAPI(String accessToken, String clientID) {
         // Create Dropbox client
-        DbxRequestConfig config = new DbxRequestConfig(clientID, "en_US");
-        dboxClient = new DbxClientV2(config, accessToken);
+        StandardHttpRequestor requestor = new StandardHttpRequestor(StandardHttpRequestor.Config.builder().build());
+        DbxRequestConfig config = DbxRequestConfig.newBuilder(clientID)
+                .withHttpRequestor(requestor)
+                .withUserLocale(Locale.getDefault().toString())
+                .build();
+//        @SuppressWarnings("deprecation") DbxRequestConfig config = new DbxRequestConfig(clientID, "en_US");
+        dropboxClient = new DbxClientV2(config, accessToken);
     }
 
     /**
      * Returns the list of all the files contained in the dropbox account
      *
-     * @return
-     * @throws DbxException
+     * @return A list of the cloud file MetaDatas
+     * @throws DbxException On error communicating with the api
      */
     public List<Metadata> getFiles() throws DbxException {
-        ListFolderBuilder listFolderBuilder = dboxClient.files().listFolderBuilder("");
+        ListFolderBuilder listFolderBuilder = dropboxClient.files().listFolderBuilder("");
         ListFolderResult result = listFolderBuilder.withRecursive(true).start();
         List<Metadata> res = new ArrayList<>();
 
@@ -44,10 +50,8 @@ public class DropBoxAPI {
             if (!result.getHasMore()) {
                 break;
             }
-
-            result = dboxClient.files().listFolderContinue(result.getCursor());
+            result = dropboxClient.files().listFolderContinue(result.getCursor());
         }
-
         return res;
     }
 
@@ -56,13 +60,13 @@ public class DropBoxAPI {
      *
      * @param localFilePath Path to the file we want to upload to the dropbox account
      * @param cloudFilePath Path to the folder in the dropbox account we want to upload the file in.
-     * @throws IOException
-     * @throws DbxException
+     * @throws IOException  On error accessing the file on disk
+     * @throws DbxException On error communicating with the api
      */
 
     public void uploadFile(String localFilePath, String cloudFilePath) throws IOException, DbxException {
         InputStream in = new FileInputStream(localFilePath);
-        UploadBuilder uploadBuilder = dboxClient.files().uploadBuilder(cloudFilePath);
+        UploadBuilder uploadBuilder = dropboxClient.files().uploadBuilder(cloudFilePath);
         uploadBuilder.withMode(WriteMode.OVERWRITE);
         uploadBuilder.uploadAndFinish(in);
         in.close();
@@ -73,13 +77,12 @@ public class DropBoxAPI {
      *
      * @param localFilePath Path of the folder we want to download our file in.
      * @param cloudFilePath Path to the file in the dropbox account where the file we want to download is.
-     * @throws IOException
-     * @throws DbxException
-     * @throws NoSuchAlgorithmException
+     * @throws IOException  on error saving the file to disk
+     * @throws DbxException On error communicating with the api
      */
-    public void downloadFile(String localFilePath, String cloudFilePath) throws IOException, DbxException, NoSuchAlgorithmException {
+    public void downloadFile(String localFilePath, String cloudFilePath) throws IOException, DbxException {
         OutputStream outputStream = new FileOutputStream(localFilePath);
-        dboxClient.files()
+        dropboxClient.files()
                 .downloadBuilder(cloudFilePath)
                 .download(outputStream);
 
@@ -89,8 +92,8 @@ public class DropBoxAPI {
     /**
      * Returns the hash value of a file.
      *
-     * @param file
-     * @return
+     * @param file File to be hashed
+     * @return The hash of the file
      */
     public String getHash(String file) {
         MessageDigest hasher = new DropBoxContentHasher();
