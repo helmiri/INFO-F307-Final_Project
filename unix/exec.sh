d6b7556078fb340c0156296624473296a8875fc0
helmiri <Hamza.El.miri@ulb.be>
2021-05-10 22:35:22 +0200

    Use GTK version 2 on linux

--- unix/exec.sh
+++ unix/exec.sh
@@ -2,17 +2,18 @@
 cd ..
 if [ $(uname -s) == "Linux" ]; then
     os="linux"
+    gtkVersion="-Djdk.gtk.version=2"
 else
     os="mac"
+    gtkVersion=""
 fi
-
 
 CLASS_PATH="./lib/sqlite/*:./lib/calendarfx/*:./lib/googledrive/*:./lib/dropbox/*:./lib/gson/*:./lib/jarchivelib/*:./lib/controlsfx/*:./lib/junit/*:./lib/junit/console/*"
 if [ "$1" == "test" ]; then
   java -cp "$CLASS_PATH:./compile/test" org.junit.platform.console.ConsoleLauncher --scan-class-path --details verbose --details-theme ascii --reports-dir ./compile/test --exclude-engine junit-vintage
 elif [ "$1" == "jar" ]; then
-  java --module-path lib/javafx/"$os" --add-modules javafx.controls,javafx.fxml,javafx.media -jar ./dist/"$2"/g06-"$2".jar
+  java --module-path lib/javafx/"$os" --add-modules javafx.controls,javafx.fxml,javafx.media $gtkVersion -jar ./dist/"$2"/g06-"$2".jar
 else
-  java --module-path lib/javafx/"$os" --add-modules javafx.controls,javafx.fxml,javafx.media -cp "$CLASS_PATH:./compile/build" be.ac.ulb.infof307.g06.Main
+  java --module-path lib/javafx/"$os" --add-modules javafx.controls,javafx.fxml,javafx.media $gtkVersion -cp "$CLASS_PATH:./compile/build" be.ac.ulb.infof307.g06.Main
 fi
 
