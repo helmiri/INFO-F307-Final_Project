d99164f7e6b4135a762693790368110d86d7a8c4
jogjini <44877150+jogjini@users.noreply.github.com>
2021-05-10 21:12:10 +0200

    Update exec.sh

--- unix/exec.sh
+++ unix/exec.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 cd ..
-
 if [ $(uname -s) == "Linux" ]; then
     os="linux"
 else
@@ -12,7 +11,8 @@ CLASS_PATH="./lib/sqlite/*:./lib/calendarfx/*:./lib/googledrive/*:./lib/dropbox/
 if [ "$1" == "test" ]; then
   java -cp "$CLASS_PATH:./compile/test" org.junit.platform.console.ConsoleLauncher --scan-class-path --details verbose --details-theme ascii --reports-dir ./compile/test --exclude-engine junit-vintage
 elif [ "$1" == "jar" ]; then
-  java --module-path lib/javafx/"$os" --add-modules javafx.controls,javafx.fxml -jar ./dist/"$2"/g06-"$2".jar
+  java --module-path lib/javafx/"$os" --add-modules javafx.controls,javafx.fxml,javafx.media -jar ./dist/"$2"/g06-"$2".jar
 else
-  java --module-path lib/javafx/"$os" --add-modules javafx.controls,javafx.fxml -cp "$CLASS_PATH:./compile/build" be.ac.ulb.infof307.g06.Main
+  java --module-path lib/javafx/"$os" --add-modules javafx.controls,javafx.fxml,javafx.media -cp "$CLASS_PATH:./compile/build" be.ac.ulb.infof307.g06.Main
 fi
+
